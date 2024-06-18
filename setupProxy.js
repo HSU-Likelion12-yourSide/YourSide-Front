571404e7b9a8363fada1f5ff2be7731ea13db408
4Bee <4bee.code@gmail.com>
2024-06-18 23:28:35 +0900

    feat: proxy의 target 설정이 유효하지 않아서 과거 CORS 충돌이 일어 났다 추측해 보았다. 이를 추후에 보완해서 API를 재접속 해봐야 겠다.

--- setupProxy.js
+++ setupProxy.js
@@ -10,3 +10,24 @@ module.exports = function (app) {
     }),
   );
 };
+
+/**
+ * target 설정에 유의해서 코드를 작성하고 proxy를 사용해야 한다.
+ const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'https://exam-lab.store/api',
+            changeOrigin: true,
+        })
+    );
+    app.use(
+        '/users',
+        createProxyMiddleware({
+            target: 'https://exam-lab.store/users',
+            changeOrigin: true,
+        })
+    );
+ */
