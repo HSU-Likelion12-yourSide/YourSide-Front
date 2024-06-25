07011f83c4e4c687d2288332167ad0d46c97acf5
4Bee <4bee.code@gmail.com>
2024-06-25 20:54:42 +0900

    feat: WorkTest와 WorkView 페이지의 post를 위한 json파일 가공

--- src/Test/Mocking/API/DevNotation.jsx
+++ src/Test/Mocking/API/DevNotation.jsx
@@ -1,19 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const DevNotation = () => {
   const [notations, setNotations] = useState([]); // 상태 초기화
 
   useEffect(() => {
     // 컴포넌트가 마운트될 때 데이터를 가져옴
-    fetch('http://localhost:3001/notations')
+    axios
+      .get('http://localhost:3001/notations')
       .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        setNotations(data); // 데이터를 상태에 저장
+        setNotations(response.data); // 데이터를 상태에 저장, response.data로 직접 접근
       })
       .catch(error => {
         console.error('There was a problem retrieving the notations:', error);
@@ -22,10 +18,10 @@ const DevNotation = () => {
 
   return (
     <div>
-      <h1>Notations</h1>
+      <h1>Notations-Get</h1>
       <ul>
         {notations.map(notation => (
-          <li key={notation.notation_id}>
+          <li key={notation.id}>
             {notation.title}
             {notation.content}
           </li>
