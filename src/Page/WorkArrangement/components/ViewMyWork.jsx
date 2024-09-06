2973b245c05301c1481f7142be60fc7f58507479
4Bee <4bee.code@gmail.com>
2024-09-06 23:36:54 +0900

    🚩: 기본적인 구조 및 css 적용

--- src/Page/WorkArrangement/components/ViewMyWork.jsx
+++ src/Page/WorkArrangement/components/ViewMyWork.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import '../css/ViewMyWork.scss';
 
 const ViewMyWork = () => {
   return (
     <div className="view-my-work">
-      <div className="vmw-">내 근로 살펴보기</div>
-      <div>
-        <div className="vmw-">
-          <div className="vmw-">코카콜라 님의 시급은 얼마입니까?</div>
-          <div className="vmw-">
-            <input type="checkbox" />
+      <div className="vmw-container">
+        <div id="vmw-title">내 근로 살펴보기</div>
+        <div className="vmw-group">
+          <div className="vmw-question">
+            <span>코카콜라 님의 시급은 얼마입니까?</span>
+            <span id="description" style={{ display: 'none' }}>
+              주 연장 근로 시간 (1일 8시간, 1주 40시간 초과한 시간)
+            </span>
+          </div>
+
+          <div className="vmw-answer">
+            <input type="input" placeholder="입력해주세요." />
             <span>원</span>
           </div>
         </div>
       </div>
-      <button className="vmw-">검사하기</button>
+      <button className="vmw-result">검사하기</button>
     </div>
   );
 };
