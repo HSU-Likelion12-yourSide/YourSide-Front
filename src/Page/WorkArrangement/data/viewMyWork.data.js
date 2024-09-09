d775939f23533d111023081a6f44eef8e2d2dac7
4Bee <4bee.code@gmail.com>
2024-09-10 00:15:57 +0900

    🐛: 리스트의 고유 key값 미적용으로 data에 id 객체를 부여하고 key값을 할당

--- src/Page/WorkArrangement/data/viewMyWork.data.js
+++ src/Page/WorkArrangement/data/viewMyWork.data.js
@@ -1,6 +1,7 @@
 const user = '코카콜라';
 const data = [
   {
+    id: '0',
     option: `${user}님의 시급은 얼마입니까?`,
     display: 'none',
     type: 'input',
@@ -9,6 +10,7 @@ const data = [
     warning: '숫자를 입력해 주세요',
   },
   {
+    id: '1',
     option: '한 주 동안 근로 시간은 몇 시간 입니까?',
     display: 'none',
     type: 'input',
@@ -17,12 +19,14 @@ const data = [
     warning: '숫자를 입력해 주세요',
   },
   {
+    id: '2',
     option: '상시 5인 이상 사업장입니까?',
     display: 'none',
     type: 'binary',
     warning: '두 체크 박스 중 하나를 선택해 주세요',
   },
   {
+    id: '3',
     option: '한 주 동안 연장 근로시간은 몇 시간  입니까?',
     display: 'flex',
     description: '주 연장 근로 시간 (1일 8시간, 1주 40시간 초과한 시간)',
@@ -32,6 +36,7 @@ const data = [
     warning: '숫자를 입력해 주세요',
   },
   {
+    id: '4',
     option: '한 주 동안 야간 근로시간이 몇 시간 입니까?',
     display: 'flex',
     description: '야간 근로 시간 (오후 10시부터 명일 오전 6시까지 근무한 시간)',
@@ -41,12 +46,16 @@ const data = [
     warning: '숫자를 입력해 주세요',
   },
   {
+    id: '5',
     option: '취업규칙 등에서 정한 약정휴일에 근무하는 시간은?',
     display: 'none',
     type: 'input',
+    placeholder: '시간을 입력해주세요.',
+    unit: '시간',
     warning: '숫자를 입력해 주세요',
   },
   {
+    id: '6',
     option: '어떤 세금이 적용되나요?',
     display: 'none',
     type: 'multi',
