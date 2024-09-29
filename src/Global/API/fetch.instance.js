617d84b4e633f247bf47e3016d30907b96b752ee
4Bee <4bee.code@gmail.com>
2024-09-29 20:57:45 +0900

    ✨: dotevn로 환경 변수 세팅 완료 및 적용

--- src/Global/API/fetch.instance.js
+++ src/Global/API/fetch.instance.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const { REACT_APP_MOCKING_API_URL } = process.env;
+
+console.log('Environment variable:', process.env.REACT_APP_MOCKING_API_URL);
+
 // Axios 인스턴스 생성
-const fetchAPI = (baseURL = 'http://localhost:3001') => {
+const fetchAPI = (baseURL = REACT_APP_MOCKING_API_URL) => {
   return axios.create({
     baseURL, // 'https://api.example.com', // API 서버의 기본 URL
     timeout: 5000, // 요청 타임아웃 설정 : 5초
