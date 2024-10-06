7c844ff9a42ce2aa4b2cfb3ee768fd196e5f9a35
[nyun-nye] <[2001_11_7@naver.com]>
2024-10-07 01:45:41 +0900

    💄: Post페이지 게시글 내용 부분 완료

--- src/Page/Post/components/Post.jsx
+++ src/Page/Post/components/Post.jsx
@@ -30,13 +30,13 @@ const Post = () => {
             </div>
           </div>
         </div>
-        <div>게시글 내용</div>
-        <div>
-          ooooo물류센터 아르바이트 하고 있는데 5월 1일 근로자의날 이나 5월6일
-          대체 공휴일 같은 날은 주휴수당이 해당 되나요? <br />
-          참고로 저날은 다 쉽니다 법정공휴일도 어떻게 되는지 궁금 합니다. <br />
-          동해물과 백두산이 마르고 닳도록 <br />
-          동해물과 백두산이 마르고 닳도록
+        <div className="post-content-container">
+          <div id="post-title">게시글 내용</div>
+          <textarea
+            className="post-content-textarea"
+            type="text"
+            placeholder="질문 내용을 입력해주세요"
+          />
         </div>
         <div>내 결과지 가져오기(선택)</div>
         <Result />
