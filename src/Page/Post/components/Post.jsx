74d22c39497e7886a8707193550effe746d2a761
[nyun-nye] <[2001_11_7@naver.com]>
2024-10-07 01:27:35 +0900

    💄 Post페이지 내부 게시글 제목, 게시판 선택 부분 완료

--- src/Page/Post/components/Post.jsx
+++ src/Page/Post/components/Post.jsx
@@ -10,23 +10,37 @@ const Post = () => {
   return (
     <div className="Post">
       <Header />
-      <div className="post-title">게시글 제목</div>
-      <div>해당 사안에도 주휴수당이 발생하는지 궁금합니다.</div>
-      <div>게시판 선택</div>
-      <div>
-        <div>네편 답변</div>
-        <div>네편 정보</div>
+      <div className="post-container">
+        <div className="post-title-container">
+          <div id="post-title">게시글 제목</div>
+          <input
+            className="post-title-input"
+            type="text"
+            placeholder="질문 제목을 입력해주세요"
+          />
+        </div>
+        <div className="post-category-container">
+          <div id="post-title">게시판 선택</div>
+          <div className="post-category-option">
+            <div className="post-input-group">
+              <input id="input-styled" type="checkbox" />
+              <span>네편 답변</span>
+              <input id="input-styled" type="checkbox" />
+              <span>네편 정보</span>
+            </div>
+          </div>
+        </div>
+        <div>게시글 내용</div>
+        <div>
+          ooooo물류센터 아르바이트 하고 있는데 5월 1일 근로자의날 이나 5월6일
+          대체 공휴일 같은 날은 주휴수당이 해당 되나요? <br />
+          참고로 저날은 다 쉽니다 법정공휴일도 어떻게 되는지 궁금 합니다. <br />
+          동해물과 백두산이 마르고 닳도록 <br />
+          동해물과 백두산이 마르고 닳도록
+        </div>
+        <div>내 결과지 가져오기(선택)</div>
+        <Result />
       </div>
-      <div>게시글 내용</div>
-      <div>
-        ooooo물류센터 아르바이트 하고 있는데 5월 1일 근로자의날 이나 5월6일 대체
-        공휴일 같은 날은 주휴수당이 해당 되나요? <br />
-        참고로 저날은 다 쉽니다 법정공휴일도 어떻게 되는지 궁금 합니다. <br />
-        동해물과 백두산이 마르고 닳도록 <br />
-        동해물과 백두산이 마르고 닳도록
-      </div>
-      <div>내 결과지 가져오기(선택)</div>
-      <Result />
       <div id="post-group">
         <div className="post-button">등록하기</div>
       </div>
