bca1b6a070bdaef1a8dabcc2ba53b54cefd17609
4Bee <4bee.code@gmail.com>
2024-10-07 20:54:14 +0900

    🐛: useFetchAPI Null type Error 발생으로 해당 코드 분석 필요

--- src/Page/WorkArrangement/components/ViewMyWorkResult.jsx
+++ src/Page/WorkArrangement/components/ViewMyWorkResult.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../css/ViewMyWorkResult.scss';
 import Header from '../../Header/components/Header';
 import Footer from '../../Footer/components/Footer';
 import VmwrResult from '../../../Global/components/VmwrResult.component';
-import data from '../../../Global/temp/data/vmwrResult.data';
+// import data from '../../../Global/temp/data/vmwrResult.data';
+import useFetchAPI from '../../../Global/API/Hooks/useFetchAPI';
 
 const Result = VmwrResult;
 
 const ViewMyWorkResult = () => {
+  const { isData, isLoading, isError, setUrl } = useFetchAPI('/results', 'GET');
+
+  useEffect(() => {
+    if (isLoading) {
+      console.log('..is Loading');
+    } else if (isError) {
+      console.log(`is Error : ${isError}`);
+    } else {
+      console.log(`Success Contact : ${isData}`);
+    }
+  });
+
   return (
     <div className="ViewMyWorkResult">
       <Header />
       <div className="vmwr-title">내 근로 결과지</div>
       <div className="vmwr-result">
-        <Result data={data} />
+        <Result data={isData} />
       </div>
       <div id="vmwr-group">
         <div className="vmwr-button">결과지 저장하기</div>
