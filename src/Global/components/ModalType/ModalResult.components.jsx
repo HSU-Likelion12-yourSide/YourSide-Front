c8c2be68e01161528a18c024b24890f8b3f1f858
4Bee <109135643+4BFC@users.noreply.github.com>
2024-10-16 12:35:58 +0900

    🔀: 각 페이지에 필요한 모달창을 구현한다. (#56)
    * 🛠 issue Template 수정
    
    깃허브에서 수정한 커밋입니다.
    
    * 🛠: pr-template 수정
    
    깃허브에서 작성한 커밋입니다.
    
    * 🛠: pr-template 수정
    
    깃허브에서 수정한 커밋입니다.
    
    * 🛠: pr-template 수정
    
    깃허브에서 작성한 커밋입니다.
    
    * 💄: modal창 기본 틀 구현 및 다른 컴포넌트에서 import 적용 후 테스트 확인
    
    * 💄: Modal창의 뒷 배경의 blur효과 추가
    
    * ✨: Modal 창 활성이 되면 scroll 비활성화 기능 구현
    
    * 💄: ViewMyWork 페이지 기준 Modal창 구현
    
    * 💄: Modal.default 부분 scss로 분리
    
    * ✨: ContextAPI로 제작한 useGlobalState로 Modal 활성화 비활성화 기능 구현 및 확인
    
    * ✨: 모달을 컨트롤 하는 함수 생성
    
    * 💄: Result에 해당하는 Modal css 분리
    
    * 💄: 저장 완료 Modal창 구현
    
    * ✨: Modal창 렌더링 순서 지정을 위한 코드 리팩토링 및 ModalType에 따른 컴포넌트 변화 추가
    
    * ✨: 공유하기 모달창 css완료 및 모달창 전환 로직 구현
    
    * 💄: Login 모달창 구현
    
    * 💄: SignUp 모달창 구현
    
    * ✨: 로그인 페이지를 Link로 이동하게 구현
    
    * 💄: div 컴포넌트에 cursor: pointer 모두 추가

---
+++ src/Global/components/ModalType/ModalResult.components.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../../css/ModalType/Modal.result.scss';
+import useGlobalState from '../../Hooks/useGlobalState';
+
+const ModalResult = () => {
+  const { setModalType } = useGlobalState();
+
+  return (
+    <div className="modal-default">
+      <div id="modal-title">결과지 이름</div>
+      <div className="modal-content">
+        <input placeholder="이름(ex. ㅁㅁ 카페)" type="text" />
+        <div id="modal-input-sub">근로 결과지</div>
+      </div>
+      <div
+        id="modal-save"
+        onKeyDown={() => {
+          console.log('test');
+        }} // 키보드 지원
+        role="button"
+        tabIndex="0"
+        onClick={() => {
+          setModalType('ResultMessage');
+        }}
+      >
+        저장하기
+      </div>
+    </div>
+  );
+};
+
+export default ModalResult;
