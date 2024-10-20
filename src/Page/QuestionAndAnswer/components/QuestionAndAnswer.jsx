ceeb418184e5bb2d82d08081cb77f1d48fc864d3
4Bee <4bee.code@gmail.com>
2024-10-20 20:13:46 +0900

    ✨: QuestionAndAnswer 페이지 post부분 컴포넌트 분리

--- src/Page/QuestionAndAnswer/components/QuestionAndAnswer.jsx
+++ src/Page/QuestionAndAnswer/components/QuestionAndAnswer.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../css/QuestionAndAnswer.scss';
-import learnMoreArrow from '../image/learnMoreArrow.svg';
 import Header from '../../Header/components/Header';
 import Footer from '../../Footer/components/Footer';
 import RankQuestion from './RankQuestion.component';
+import Question from './Question.component';
 
 const App = () => {
   return (
     <div className="question-and-answer">
       <Header />
       <div className="qa-group">
-        <div id="qa-box" className="selected">
+        <div id="qa-box-selected" className="selected">
           네편 답변
         </div>
         <div id="qa-box">네편 정보</div>
@@ -19,32 +19,8 @@ const App = () => {
         <div id="qa-popular-intro">인기게시글</div>
         <div className="qa-popular-group">
           <RankQuestion />
-          <div className="qa-popular-box">
-            <div className="qa-popular-question">
-              <div id="qa-popular-mark">Q.</div>
-              <div id="qa-popular-title">
-                해당 사안에도 주휴수당이 발생하는지 궁금합니다
-              </div>
-            </div>
-            <div id="qa-popular-text">
-              물류센터 아르바이트 하고 있는데 5월1일 근로자의날 이나 5월6일 대체
-              공휴일 같은 날은 주휴수당이 해당 되나요? 참고로 저날은 다 쉽니다
-              법정공휴일도 어떻게 되는지 궁금 합니다.
-            </div>
-          </div>
-          <div className="qa-popular-box">
-            <div className="qa-popular-question">
-              <div id="qa-popular-mark">Q.</div>
-              <div id="qa-popular-title">
-                해당 사안에도 주휴수당이 발생하는지 궁금합니다
-              </div>
-            </div>
-            <div id="qa-popular-text">
-              물류센터 아르바이트 하고 있는데 5월1일 근로자의날 이나 5월6일 대체
-              공휴일 같은 날은 주휴수당이 해당 되나요? 참고로 저날은 다 쉽니다
-              법정공휴일도 어떻게 되는지 궁금 합니다.
-            </div>
-          </div>
+          <RankQuestion />
+          <RankQuestion />
         </div>
       </div>
       <div className="qa-post-container">
@@ -53,78 +29,9 @@ const App = () => {
           <div id="qa-write">게시글 작성</div>
         </div>
         <div className="qa-post-group">
-          <div className="qa-post-card">
-            <div className="qa-left-group">
-              <div className="qa-post-title">
-                <div id="qa-post-mark">Q.</div>
-                <div id="qa-post-question">
-                  계약서 쓴 날짜 보다 미리 와서 2시간 있다갔어요.
-                </div>
-              </div>
-              <div className="qa-post-text">
-                물류센터 아르바이트 하고 있는데 5월1일 근로자의날 이나 5월6일
-                대체 공휴일 같은 날은 주휴수당이 해당 되나요? 참고로 저날은 다
-                쉽니다 법정공휴일도 어떻게 되는지 궁금 합니다.
-              </div>
-            </div>
-            <div className="qa-right-group">
-              <div id="qa-post-date">2024.04.11</div>
-              <div className="qa-learn-more">
-                <div id="qa-learn-more-text">자세히 보기</div>
-                <div>
-                  <img src={learnMoreArrow} alt="qa-learn-more" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="qa-post-card">
-            <div className="qa-left-group">
-              <div className="qa-post-title">
-                <div id="qa-post-mark">Q.</div>
-                <div id="qa-post-question">
-                  계약서 쓴 날짜 보다 미리 와서 2시간 있다갔어요.
-                </div>
-              </div>
-              <div className="qa-post-text">
-                물류센터 아르바이트 하고 있는데 5월1일 근로자의날 이나 5월6일
-                대체 공휴일 같은 날은 주휴수당이 해당 되나요? 참고로 저날은 다
-                쉽니다 법정공휴일도 어떻게 되는지 궁금 합니다.
-              </div>
-            </div>
-            <div className="qa-right-group">
-              <div id="qa-post-date">2024.04.11</div>
-              <div className="qa-learn-more">
-                <div id="qa-learn-more-text">자세히 보기</div>
-                <div>
-                  <img src={learnMoreArrow} alt="qa-learn-more" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="qa-post-card">
-            <div className="qa-left-group">
-              <div className="qa-post-title">
-                <div id="qa-post-mark">Q.</div>
-                <div id="qa-post-question">
-                  계약서 쓴 날짜 보다 미리 와서 2시간 있다갔어요.
-                </div>
-              </div>
-              <div className="qa-post-text">
-                물류센터 아르바이트 하고 있는데 5월1일 근로자의날 이나 5월6일
-                대체 공휴일 같은 날은 주휴수당이 해당 되나요? 참고로 저날은 다
-                쉽니다 법정공휴일도 어떻게 되는지 궁금 합니다.
-              </div>
-            </div>
-            <div className="qa-right-group">
-              <div id="qa-post-date">2024.04.11</div>
-              <div className="qa-learn-more">
-                <div id="qa-learn-more-text">자세히 보기</div>
-                <div>
-                  <img src={learnMoreArrow} alt="qa-learn-more" />
-                </div>
-              </div>
-            </div>
-          </div>
+          <Question />
+          <Question />
+          <Question />
         </div>
       </div>
       <Footer />
