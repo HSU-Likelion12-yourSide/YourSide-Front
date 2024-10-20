4bff9592bb4e89b5a03d678c6e8c9dea6bfc9494
4Bee <4bee.code@gmail.com>
2024-10-20 23:33:12 +0900

    ✨: QuestionAndAnswer 페이지의 게시글 작성 페이지를 navigate QnAPosting페이지로 넘어가게 지정, Alert으로 표기

--- src/Page/QuestionAndAnswer/components/QuestionAndAnswer.jsx
+++ src/Page/QuestionAndAnswer/components/QuestionAndAnswer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../css/QuestionAndAnswer.scss';
+import { useNavigate } from 'react-router-dom';
+import navigateController from '../../../Global/function/navigateController';
 import Header from '../../Header/components/Header';
 import Footer from '../../Footer/components/Footer';
 import RankQuestion from './RankQuestion.component';
 import Question from './Question.component';
 
-const App = () => {
+const QuestionAndAnswer = () => {
+  const navigate = useNavigate();
   return (
     <div className="question-and-answer">
       <Header />
@@ -26,7 +29,19 @@ const App = () => {
       <div className="qa-post-container">
         <div className="qa-post-intro">
           <div id="qa-post-word">게시글</div>
-          <div id="qa-write">게시글 작성</div>
+          <div
+            id="qa-write"
+            onKeyDown={() => {}}
+            onClick={() => {
+              /* eslint-disable-next-line no-alert */
+              alert('게시글 작성 페이지는 준비 중입니다.');
+              navigateController(navigate, '/QnAPosing');
+            }}
+            role="button"
+            tabIndex="0"
+          >
+            게시글 작성
+          </div>
         </div>
         <div className="qa-post-group">
           <Question />
@@ -39,4 +54,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default QuestionAndAnswer;
