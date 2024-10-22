e3a865235f7b6485ad6d1e682573277a75916d4c
4Bee <4bee.code@gmail.com>
2024-10-22 17:08:08 +0900

    🐛 : VmwrResult의 근로결과지 API연결 VmwrOptionButton 컴포넌트 확인 필요 true,false값이 잘 넘어가지지 않음

--- src/Global/components/VmwrResult.component.jsx
+++ src/Global/components/VmwrResult.component.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-// import VmwrOptionButton from '../../Page/WorkArrangement/components/VmwrButton.component';
+import VmwrOptionButton from '../../Page/WorkArrangement/components/VmwrButton.component';
 import '../css/VmwrResult.component.scss';
 // * data를 해당 컴포넌트를 사용하는 최상위 컴포넌트로 부터 props를 받아오게 코드 리팩토링
 // import data from '../temp/data/vmwrResult.data';
@@ -58,50 +58,63 @@ const VmwrResult = ({ resultId }) => {
 
   // * 여기서 부터 함수로 분리가 필요 논의가 필요
   // 해당 데이터를 API로 받아오기 때문에 Props로 받아올 수 있게 구조를 다시 검토하고 재구성 필요
-  // const OptionsResultData = data;
-  // const ResultContents = OptionsResultData[0].data.content
-  //   .trim()
-  //   .split('니다.')
-  //   .filter(el => el !== ''); // 빈 문자열 제거
+  // 데이터 검증이 필요
+  let OptionsResultData = '';
+  let ResultContents = '';
+  let resultState = [];
+  if (isData && isData.data) {
+    OptionsResultData = isData;
+    ResultContents = OptionsResultData.data.content
+      .trim()
+      .split('니다.')
+      .filter(el => el !== ''); // 빈 문자열 제거
+    resultState = [
+      OptionsResultData.data.extraPay,
+      OptionsResultData.data.weekPay,
+      OptionsResultData.data.nightPay,
+      OptionsResultData.data.overtimePay,
+      OptionsResultData.data.holidayPay,
+    ];
+  } else {
+    // 데이터가 없을 경우 기본값 설정
+    resultState = [false, false, false, false, false];
+  }
 
-  // const OptionsList = [
-  //   '가산수당',
-  //   '주휴수당',
-  //   '야간근로수당',
-  //   '연장근로수당',
-  //   '휴일근로수당',
-  // ];
+  const OptionsList = [
+    '가산수당',
+    '주휴수당',
+    '야간근로수당',
+    '연장근로수당',
+    '휴일근로수당',
+  ];
 
   // 결과 상태 데이터 가공 배열
-  // const resultState = [
-  //   OptionsResultData[0].data.extraPay,
-  //   OptionsResultData[0].data.weekPay,
-  //   OptionsResultData[0].data.nightPay,
-  //   OptionsResultData[0].data.overtimePay,
-  //   OptionsResultData[0].data.holidayPay,
-  // ];
 
   return (
     <div className="vmwr-container">
       <div className="vmwr-contents">
         <div className="vmwr-result-title">근로 결과지</div>
-        {/* <div>
-          {ResultContents.map(el => {
-            return <div key={el}>{el}니다.</div>;
-          })}
-        </div> */}
+        <div>
+          {isData &&
+            isData.data &&
+            ResultContents.map(el => {
+              return <div key={el}>{el}니다.</div>;
+            })}
+        </div>
         {/* <div>{content}</div> */}
       </div>
       <div className="vmwr-group">
         <div className="vmwr-options">발생 요건들</div>
         <div className="vmwr-list">
-          {/* {OptionsList.map((option, index) => (
-            <VmwrOptionButton
-              key={option}
-              resultState={resultState[index] ? 'check' : 'uncheck'}
-              option={option}
-            />
-          ))} */}
+          {isData &&
+            isData.data &&
+            OptionsList.map((option, index) => (
+              <VmwrOptionButton
+                key={option}
+                resultState={resultState[index] ? 'check' : 'uncheck'}
+                option={option}
+              />
+            ))}
         </div>
       </div>
     </div>
