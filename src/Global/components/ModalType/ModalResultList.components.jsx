f5bc9bb9df90aa4bd5657bafb0443a8acccfbe3c
[nyun-nye] <[2001_11_7@naver.com]>
2024-10-27 00:10:29 +0900

    ✨: 기본적인 레이아웃 생성

--- src/Global/components/ModalType/ModalResultList.components.jsx
+++ src/Global/components/ModalType/ModalResultList.components.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../css/ModalType/Modal.resultlist.scss';
 import useGlobalState from '../../Hooks/useGlobalState';
-import closeIcon from './image.png';
+import closeIcon from './closeIcon.svg';
 
 const ModalResult = () => {
   const { setModalType } = useGlobalState();
@@ -10,23 +10,27 @@ const ModalResult = () => {
     <div className="modal-default">
       <div id="modal-header">
         <div id="modal-title">나의 결과지</div>
-        <div id="modal-get">가져오기</div>
+        <div
+          id="modal-get"
+          onKeyDown={() => {
+            console.log('test');
+          }}
+          role="button"
+          tabIndex="0"
+        >
+          가져오기
+        </div>
         <div id="modal-close">
           <img src={closeIcon} alt="x" id="modal-closeicon" />
         </div>
       </div>
-      <div
-        id="modal-save"
-        onKeyDown={() => {
-          console.log('test');
-        }} // 키보드 지원
-        role="button"
-        tabIndex="0"
-        onClick={() => {
-          setModalType('ResultMessage');
-        }}
-      >
-        저장하기
+      <div id="modal-contents">
+        {[...Array(5)].map((_, index) => (
+          <div key={index} id="modal-resultbox">
+            <div id="modal-resultname">미도인 성수 근로 결과지</div>
+            <div id="modal-viewdetails">자세히 보기 &gt;</div>
+          </div>
+        ))}
       </div>
     </div>
   );
