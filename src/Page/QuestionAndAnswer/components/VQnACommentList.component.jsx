db81076704b57db867aa19976c092f3d5a54eef4
4Bee <4bee.code@gmail.com>
2024-10-28 16:10:47 +0900

    ✨: 댓글 달기위한 API post 완료

--- src/Page/QuestionAndAnswer/components/VQnACommentList.component.jsx
+++ src/Page/QuestionAndAnswer/components/VQnACommentList.component.jsx
@@ -5,11 +5,24 @@ import useFetchAPI from '../../../Global/API/Hooks/useFetchAPI';
 import '../css/VQnACommentList.scss';
 
 const VQnACommentList = () => {
+  const [isComment, setComment] = useState();
+  const [isContent, setContent] = useState('');
+
+  // 해당 user_id, posting_id 다시 설정 필요
   const requestData = {
     // eslint-disable-next-line camelcase
     user_id: 1, // 현재 조회하는 회원의 고유 ID
     // eslint-disable-next-line camelcase
     posting_id: 2, // 댓글 고유 ID
+  };
+
+  // 해당 user_id, posting_id 다시 설정 필요
+  const requestCommentData = {
+    // eslint-disable-next-line camelcase
+    user_id: 1,
+    // eslint-disable-next-line camelcase
+    posting_id: 2,
+    content: isComment,
   };
 
   // GET API 수정 예정
@@ -19,22 +32,56 @@ const VQnACommentList = () => {
     requestData,
   );
 
-  const [isContent, setContent] = useState('');
+  const {
+    isData: isCommentData,
+    isLoading: isCommentLoading,
+    isError: isCommentError,
+    setUrl: setCommentUrl,
+  } = useFetchAPI('', 'POST', requestCommentData);
+
+  const postComment = () => {
+    if (isComment.trim()) {
+      setCommentUrl('/comment'); // 답변이 있을 때만 URL을 설정하여 POST 요청 발생
+    } else {
+      // alert('댓글 내용을 입력해주세요.');
+    }
+  };
 
   useEffect(() => {
-    if (isLoading) {
+    if (isLoading || isCommentLoading) {
       console.log('..is Loading');
       setContent('Loading...');
-    } else if (isError) {
-      console.log(`is Error : `, isError);
-      setContent(`Error: `, isError);
-    } else if (isData && isData.data) {
-      console.log(`Success Contact : `, isData);
-      setContent(isData.data);
+    } else if (isError || isCommentError) {
+      console.log(`Error: `, isError || isCommentError);
+      setContent(`Error: ${isError || isCommentError}`);
     } else {
-      setContent(null);
+      if (isData && isData.data) {
+        console.log(`Success GET Contact: `, isData);
+        setContent(isData.data);
+      }
+      if (isCommentData && isCommentData.data) {
+        console.log(`Success POST Comment: `, isCommentData);
+        setContent(isCommentData.data);
+      }
     }
-  }, [isLoading, isError, isData]);
+  }, [
+    isLoading,
+    isCommentLoading,
+    isError,
+    isCommentError,
+    isData,
+    isCommentData,
+  ]);
+
+  useEffect(() => {
+    // !!필 요청이 완료된 후 commentUrl 초기화
+    if (isCommentData) {
+      console.log(`Success POST Comment: `, isCommentData);
+      setContent(isCommentData.data);
+      setCommentUrl(null); // 초기화하여 재요청 방지
+      setComment(''); // 요청 후 입력란 비우기
+    }
+  }, [isCommentData]);
 
   // 임시
   // useEffect(() => {
@@ -81,9 +128,23 @@ const VQnACommentList = () => {
             id="qav-comment-textfield"
             type="text"
             placeholder="답변을 남겨주세요"
+            value={isComment}
+            onChange={e => setComment(e.target.value)}
           />
           <div className="qav-button-group">
-            <div id="qav-comment-button">답변 달기</div>
+            <div
+              id="qav-comment-button"
+              onKeyDown={() => {}}
+              onClick={() => {
+                console.log('click');
+                postComment();
+              }}
+              role="button"
+              tabIndex="0"
+              aria-label="comment-btn"
+            >
+              답변 달기
+            </div>
           </div>
         </div>
       </div>
