3fa3b879b1a77bfb01561e27b755a78ab6061303
4Bee <109135643+4BFC@users.noreply.github.com>
2024-11-01 14:16:11 +0900

    🔀: 네편현답 상세 보기 (ViewQuestionAndAnswer) 페이지 컴포넌트 분리와 코멘트 API를 연결한다. (#74)
    * 🛠 issue Template 수정
    
    깃허브에서 수정한 커밋입니다.
    
    * 🛠: pr-template 수정
    
    깃허브에서 작성한 커밋입니다.
    
    * 🛠: pr-template 수정
    
    깃허브에서 수정한 커밋입니다.
    
    * 🛠: pr-template 수정
    
    깃허브에서 작성한 커밋입니다.
    
    * 💄: modal창 기본 틀 구현 및 다른 컴포넌트에서 import 적용 후 테스트 확인
    
    * 💄: Modal창의 뒷 배경의 blur효과 추가
    
    * ✨: Modal 창 활성이 되면 scroll 비활성화 기능 구현
    
    * 💄: ViewMyWork 페이지 기준 Modal창 구현
    
    * 💄: Modal.default 부분 scss로 분리
    
    * ✨: ContextAPI로 제작한 useGlobalState로 Modal 활성화 비활성화 기능 구현 및 확인
    
    * ✨: 모달을 컨트롤 하는 함수 생성
    
    * 💄: Result에 해당하는 Modal css 분리
    
    * 💄: 저장 완료 Modal창 구현
    
    * ✨: Modal창 렌더링 순서 지정을 위한 코드 리팩토링 및 ModalType에 따른 컴포넌트 변화 추가
    
    * ✨: 공유하기 모달창 css완료 및 모달창 전환 로직 구현
    
    * 💄: Login 모달창 구현
    
    * 💄: SignUp 모달창 구현
    
    * ✨: 로그인 페이지를 Link로 이동하게 구현
    
    * 💄: div 컴포넌트에 cursor: pointer 모두 추가
    
    * ✨: 네편 답변 페이지, 네편 정보 페이지에 따른 API 통합 구현
    
    * ✨: API로 부터 가져오는 데이터 PropsType 설정
    
    * ✨: Question 컴포넌트에 게시글 API 전체 조회 API 연결 및 map으로 동작할 수 있게 루프 로직 구현
    
    * ✨: 인기 게시글 조회 완료 및 두개의 API를 가져올 수 있게 코드 구현
    
    * ✨: isLoading일 때는 Loading 글자만 보이게 구현 상단 네편 답변, 네편 정보를 isType 상태 값에 따라 selected scss가 적용되게 설정
    
    * ✨: QandA 목록 상세보기 클릭시 해당 게시글 id 데이터를 전역 상태 값을 주기 위해 상태 값 추가
    
    * ✨: 전역을 설정한 데이터 값을 기준으로 API 연동 확인 및 근로결과지 worksheetid값 및 데이터 예외처리
    
    * ✨: 해당 게시글의 근로결과지 props로 받을 수 있게 변경
    
    * 🐛 : VmwrResult의 근로결과지 API연결 VmwrOptionButton 컴포넌트 확인 필요 true,false값이 잘 넘어가지지 않음
    
    * ✨: bookmark post API 필요 확인
    
    * ✨: VmwrResult의 옵션들 정상적으로 동작 확인 및 새로고침 시 데이터 손실 확인
    
    * ✨: 동적 경로로 주소 설정
    
    * ✨: 임시로 사용할 package에 Proxy 사용
    
    * 🔥: Proxy 설정 제거, 주소 노출 및 위험이 있어 제거
    
    * ✨: 동적으로 받아오는 URI의 일부 데이터를 useParams로 추출, CORS 정책 위반으로 백엔드와 논의 필요
    
    * 🐛: useParams로 URI 값 도출하는 문제 해결
    
    * ✨: bookMark 상태 변화에 따른 UI 변동 적용
    
    * ✨: 비구조 할당으로 정의한 useEffect로 북마트 API Post연결 완료
    
    * ✨: commentList 영역 컴포넌트로 분리
    
    * ✨: comment 전체 조회를 현재 querystring으로 구현. Get requestbody로 구현할 경우 reject가 되는 error 확인
    
    * ✨: 코멘트 bad, Like 표기 및 API 완료
    
    * ✨: 댓글 달기위한 API post 완료
    
    * ✨: post 테스트를 위한 커밋

--- src/Page/QuestionAndAnswer/components/ViewQuestionAndAnswer.jsx
+++ src/Page/QuestionAndAnswer/components/ViewQuestionAndAnswer.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import '../css/ViewQuestionAndAnswer.scss';
 import { useNavigate, useParams } from 'react-router-dom';
-import likeButton from '../image/like-btn.svg';
-import badButton from '../image/bad-btn.svg';
 import Header from '../../Header/components/Header';
 import Footer from '../../Footer/components/Footer';
+import CommentList from './VQnACommentList.component';
 import useFetchAPI from '../../../Global/API/Hooks/useFetchAPI';
 import VmwrResult from '../../../Global/components/VmwrResult.component';
 import navigateController from '../../../Global/function/navigateController';
@@ -136,64 +135,7 @@ const ViewQuestionAndAnswer = () => {
             </div>
           </div>
         </div>
-        <div className="qav-write-comment">
-          <div className="qav-group">
-            <div id="qav-comment-writer">코카콜라</div>
-            <textarea
-              cols="80"
-              id="qav-comment-textfield"
-              type="text"
-              placeholder="답변을 남겨주세요"
-            />
-            <div className="qav-button-group">
-              <div id="qav-comment-button">답변 달기</div>
-            </div>
-          </div>
-        </div>
-        <div className="qav-comment-array">
-          <div className="qav-comment-array-group">
-            <div id="qav-comment-array-method">좋아요순</div>
-            <div id="qav-comment-array-method">최신순</div>
-          </div>
-          <div className="qav-comment">
-            <div className="qav-group">
-              <div className="qav-comment-writer">
-                <span id="qav-writer">사장나와</span>
-                {/* 등급 부분 */}
-                <div id="user-rank">네편 노무사</div>
-              </div>
-              <div className="qav-users-group">
-                <div id="qav-users-comment">
-                  기간제 근로자의 경우, 약정한 근로계약 기간이 만료됨에 따라
-                  근로관계가 자동으로 종료되는 것이 원칙입니다. 즉, 기간만료에
-                  따라 근로관계가 종료되는 것은 해고가 아니므로, 사용자가 기간제
-                  근로자에게 계약기간 만료 여부를 미리 통지할 의무는 해당
-                  사업장의 해당 사업장의 취업규칙이나 근로계약서에 계약기간 만료
-                  전에 별도의 의사를 표시하지 않으면 자동으로 근로계약이
-                  연장된다와 같은 근로계약 자동연장에 관한 조항이 있거나,
-                  계약기간 만료 oo일 전에 계약연장 여부를 통지한다와 같은 규정이
-                  명시되어 있다면, 해당 내용을 준수하여 사전에 계약만료 또는
-                  계약연장에 관한 통지를 하여야 합니다.
-                </div>
-                <div className="qav-users-button">
-                  <div id="qav-button-like-group">
-                    <img src={likeButton} alt="좋아요" />
-                    <div className="qav-comment-like">
-                      <div id="qav-comment-like-count">999</div>
-                    </div>
-                  </div>
-                  <div id="qav-button-bad-group">
-                    <img src={badButton} alt="나빠요" />{' '}
-                    <div className="qav-comment-bad">
-                      <div id="qav-comment-bad-count">999</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div id="qav-comment-date">2024.04.11</div>
-            </div>
-          </div>
-        </div>
+        <CommentList />
       </div>
       <Footer />
     </div>
