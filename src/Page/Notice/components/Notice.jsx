86093a4ad38eb374c263641bb749e2a6c79c8d2e
4Bee <4bee.code@gmail.com>
2024-11-06 18:20:56 +0900

    ✨: 공지사항(Notice) 페이지 API 연동 구현, 추후 더미 데이터 추가 될 시 구현 필요

--- src/Page/Notice/components/Notice.jsx
+++ src/Page/Notice/components/Notice.jsx
@@ -1,5 +1,5 @@
 // import React, { useState } from 'react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import '../css/Notice.css';
 import '../css/Notice.scss';
 import ArrowLeft from '../img/page-left-arrow.svg';
@@ -7,11 +7,30 @@ import ArrowRight from '../img/page-right-arrow.svg';
 import Header from '../../Header/components/Header';
 import Footer from '../../Footer/components/Footer';
 import NoticeElement from './NoticeElement.component';
+import useFetchAPI from '../../../Global/API/Hooks/useFetchAPI';
 
 const Notice = () => {
   // const [isPage, setPage] = useState(1); // 페이지 네이션 시작 기본 설정
   // const limit = 5;
   // const offset = (isPage - 1) * limit;
+  const [isContent, setContent] = useState(''); // 렌더링할 content 상태 관리 content
+  const { isData, isLoading, isError, setUrl } = useFetchAPI('/notation/list');
+  // GET API 상태 표기
+  useEffect(() => {
+    if (isLoading) {
+      console.log('..is Loading');
+      setContent('Loading...');
+    } else if (isError) {
+      console.log(`is Error : `, isError);
+      setContent(`Error: `, isError);
+    } else if (isData && isData.data) {
+      console.log(`Success Contact : `, isData);
+      setContent(isData.data);
+      // setContent();
+    } else {
+      setContent(null);
+    }
+  }, [isLoading, isError, isData]);
 
   return (
     <div>
@@ -20,7 +39,11 @@ const Notice = () => {
         <div className="Notice-container">
           <div id="Notice-title">공지사항</div>
           <div className="Notice-list">
-            <NoticeElement />
+            {isData && isData.data ? (
+              <NoticeElement />
+            ) : (
+              <div id="Notice-no-data">현재 등록된 공지사항이 없습니다.</div>
+            )}
           </div>
           <div className="Notice-pagination">
             <div id="Notice-pagination-arrow">
