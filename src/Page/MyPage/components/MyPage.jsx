46f1cbb7cd4291745f6760408cb8dd37513ff451
4Bee <4bee.code@gmail.com>
2024-11-09 18:51:46 +0900

    ✨: MyPage Contents영역 동적으로 선택된 부분 기능 구현

--- src/Page/MyPage/components/MyPage.jsx
+++ src/Page/MyPage/components/MyPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/MyPage.scss';
 import Header from '../../Header/components/Header';
 import Footer from '../../Footer/components/Footer';
 
 const MyPage = () => {
+  const [isContentState, setContentState] = useState(false);
+  const [isContent, setContent] = useState({
+    myResult: true,
+    myPosting: false,
+    myComment: false,
+    myBookMark: false,
+  });
+
+  const handleContent = contentType => {
+    setContent(prev => ({
+      ...prev,
+      myResult: contentType === 'myResult',
+      myPosting: contentType === 'myPosting',
+      myComment: contentType === 'myComment',
+      myBookMark: contentType === 'myBookMark',
+    }));
+  };
+
   return (
     <div className="MyPage">
       <Header />
@@ -26,13 +44,63 @@ const MyPage = () => {
           </div>
         </div>
         <div className="MyPage-switch">
-          <div id="">내 결과지</div>
-          <div id="">내 게시글</div>
-          <div id="">내 답변</div>
-          <div id="">책갈피</div>
+          <div
+            id={isContent.myResult ? 'MyPage-switched' : ''}
+            onKeyDown={() => {}}
+            onClick={() => {
+              handleContent('myResult');
+            }}
+            tabIndex="0"
+            role="button"
+          >
+            내 결과지
+          </div>
+          <div
+            id={isContent.myPosting ? 'MyPage-switched' : ''}
+            onKeyDown={() => {}}
+            onClick={() => {
+              handleContent('myPosting');
+            }}
+            tabIndex="0"
+            role="button"
+          >
+            내 게시글
+          </div>
+          <div
+            id={isContent.myComment ? 'MyPage-switched' : ''}
+            onKeyDown={() => {}}
+            onClick={() => {
+              handleContent('myComment');
+            }}
+            tabIndex="0"
+            role="button"
+          >
+            내 답변
+          </div>
+          <div
+            id={isContent.myBookMark ? 'MyPage-switched' : ''}
+            onKeyDown={() => {}}
+            onClick={() => {
+              handleContent('myBookMark');
+            }}
+            tabIndex="0"
+            role="button"
+          >
+            책갈피
+          </div>
         </div>
-        <div className="MyPage-contents">contents 변경 컴포넌트</div>
-        <div className="MyPage-pagenation">페이지 네이션</div>
+        {isContentState ? (
+          <div className="MyPage-contents">contents 변경 컴포넌트</div>
+        ) : (
+          // 해당 부분 질문 부분에 동적으로 변경이 가능하도록 구현해야한다.
+          <div className="MyPage-none-contents">
+            작성한 질문(글)이 없습니다.
+          </div>
+        )}
+        {/* 페이지 네이션은 각 컴폰넌트 내부로 들어갈 예정이다. */}
+        <div className="MyPage-pagenation">
+          {isContentState ? '페이지 네이션' : ''}
+        </div>
       </div>
       <Footer />
     </div>
