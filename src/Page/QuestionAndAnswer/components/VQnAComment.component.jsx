1b7bdf6832356d5f0ce7b4b665b05ddf30f7ced2
4Bee <109135643+4BFC@users.noreply.github.com>
2024-11-12 02:23:24 +0900

    🔀: 네편현답 상세 보기 (ViewQuestionAndAnswer) 페이지 코멘트 영역의 프레임 드랍 현상을 해결한다. (#90)
    * ✨: 코멘트 부분 API연결 완료, CSS 재검토 필요
    
    * 🔥: 불필요한 코드, 파일 제거
    
    * ✨: 좋아요 싫어요 API 연결 및 구현
    
    * 🚑: React의 비동기 처리와 API 브라우저 비동기 처리간의 로직 혼돈으로 좋아요, 싫어요 버튼 클릭시 원할하게 동작하지 못하고 있음
    
    * 🚑: Get과 Post를 동시 다발 적으로 구현해서 발생한 오류, 최적화 오류로 프레임 드랍 현상 발생
    
    * 🐛:  코멘트 API 부분에 발생하는 문제점을 프론트 회의를 통해 이루어진 시나리오로 코드 수정

--- src/Page/QuestionAndAnswer/components/VQnAComment.component.jsx
+++ src/Page/QuestionAndAnswer/components/VQnAComment.component.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import '../css/VQnAComment.scss';
-import useFetchAPI from '../../../Global/API/Hooks/useFetchAPI';
+// import useFetchAPI from '../../../Global/API/Hooks/useFetchAPI';
 
 const VQnAComment = ({
   userId,
@@ -14,13 +14,15 @@ const VQnAComment = ({
   isLiked,
   likeCount,
   dislikeCount,
-  triggerReload,
+  // triggerReload,
 }) => {
   // 나의 버튼 상태 값
   const [isLike, setLiked] = useState(isLiked);
   const [isBad, setBad] = useState(isDisLiked);
-  // const [isLikedCount, setLikedCount] = useState(likeCount);
-  // const [isBadCount, setBadCount] = useState(dislikeCount);
+  const [isCurrentLiked, setCurrentLiked] = useState(false);
+  const [isCurrentBad, setCurrentBad] = useState(false);
+  const [isLikedCount, setLikedCount] = useState(likeCount);
+  const [isBadCount, setBadCount] = useState(dislikeCount);
   const [isContent, setContent] = useState(''); // 렌더링할 content 상태 관리 content
   // lint 문법을 피하기 위한 log
   // eslint-disable-next-line camelcase
@@ -29,6 +31,7 @@ const VQnAComment = ({
     console.log(isDisLiked);
   }
 
+  /**
   const requestLike = {
     // eslint-disable-next-line camelcase
     user_id: 2,
@@ -62,40 +65,63 @@ const VQnAComment = ({
     isError: isBadError,
     setUrl: setBadUrl,
   } = useFetchAPI('', 'POST', requestBad);
-
+ */
   const likeButtonController = () => {
-    setLiked(prev => !prev); // 상태 반전
-    // setLiked(isLike);
-    setLikeUrl('/comment/likes');
-    triggerReload();
-    console.log(`현재 좋아요는 ${isLike}`);
+    if (isCurrentLiked) {
+      // eslint-disable-next-line no-alert
+      alert('방금 클릭하셨습니다.');
+      window.location.reload(); // alert 확인 후 페이지 새로고침
+      return null;
+    }
+    setCurrentLiked(true);
+    setCurrentBad(false);
+
+    // 좋아요 카운트 증가
+    setLikedCount(prev => prev + 1);
+
+    // 싫어요가 활성화된 상태였다면 싫어요 카운트 감소
+    if (isCurrentBad) {
+      setBadCount(prev => prev - 1);
+    }
+
+    return null;
   };
 
   const badButtonController = () => {
-    setBad(prev => !prev); // 상태 반전
-    // setBad(isBad);
-    // setBadCount(isBadCount + 1);
-    setBadUrl('/comment/dislikes');
-    triggerReload();
-    console.log(`현재 싫어요는 ${isBad}`);
+    if (isCurrentBad) {
+      // eslint-disable-next-line no-alert
+      alert('방금 클릭하셨습니다.');
+      window.location.reload(); // alert 확인 후 페이지 새로고침
+      return null;
+    }
+    setCurrentLiked(false);
+    setCurrentBad(true);
+
+    // 싫어요 카운트 증가
+    setBadCount(prev => prev + 1);
+
+    // 좋아요가 활성화된 상태였다면 좋아요 카운트 감소
+    if (isCurrentLiked) {
+      setLikedCount(prev => prev - 1);
+    }
+
+    return null;
   };
 
-  // useEffect(() => {
-  //   // isLike 상태 변경 시 트리거
-  //   if (isLike) {
-  //     triggerReload();
-  //   }
-  // }, [isLike]);
-
-  // useEffect(() => {
-  //   // isBad 상태 변경 시 트리거
-  //   if (isBad) {
-  //     triggerReload();
-  //   }
-  // }, [isBad]);
-
   useEffect(() => {
-    triggerReload();
+    console.log(`like ${isCurrentLiked}`);
+    console.log(`bad ${isCurrentBad}`);
+  }, [
+    setCurrentLiked,
+    setCurrentBad,
+    likeButtonController,
+    badButtonController,
+  ]);
+
+  /**
+  // API useEffect
+  useEffect(() => {
+    console.log(`API Like: ${isLiked} Bad: ${isDisLiked}`);
     if (isLikeLoading || isBadLoading) {
       console.log('..is Loading');
       setContent('Loading...');
@@ -117,13 +143,13 @@ const VQnAComment = ({
     isLikeData,
     isBadData,
   ]);
-
+ */
   return (
     <div className="qav-comment">
       <div className="qav-group">
         <div className="qav-comment-writer">
           <span id="qav-writer">
-            {nickName === 'null' ? nickName : 'TestName'}
+            {nickName === 'null' ? nickName : '코카콜라'}
           </span>
           {/* 등급 부분 미제작 */}
           {/* <div id="user-rank">네편 노무사</div> */}
@@ -133,7 +159,11 @@ const VQnAComment = ({
           <div className="qav-users-button">
             <div id="qav-button-like-group">
               <div
-                id={isLiked ? 'qav-clicked-like' : 'qav-like-button'}
+                id={
+                  isCurrentLiked || isLiked
+                    ? 'qav-clicked-like'
+                    : 'qav-like-button'
+                }
                 onKeyDown={() => {}}
                 onClick={() => {
                   likeButtonController();
@@ -144,14 +174,18 @@ const VQnAComment = ({
               />
               <div className="qav-comment-like">
                 {/* <div id="qav-comment-like-count">{likeCount}</div> */}
-                <div id="qav-comment-like-count">{likeCount}</div>
+                <div id="qav-comment-like-count">{isLikedCount}</div>
               </div>
             </div>
             <div id="qav-button-bad-group">
               <div
                 // API 점검 필요로 API에 따른 boolean 값으로 확인하지 않고 로컬 boolean 값으로 지정
                 // id={isDisLiked ? 'qav-clicked-bad' : 'qav-bad-button'}
-                id={isDisLiked ? 'qav-clicked-bad' : 'qav-bad-button'}
+                id={
+                  isCurrentBad || isDisLiked
+                    ? 'qav-clicked-bad'
+                    : 'qav-bad-button'
+                }
                 onKeyDown={() => {}}
                 onClick={() => {
                   badButtonController();
@@ -162,7 +196,7 @@ const VQnAComment = ({
               />
               <div className="qav-comment-bad">
                 {/* <div id="qav-comment-bad-count">{dislikeCount}</div> */}
-                <div id="qav-comment-bad-count">{dislikeCount}</div>
+                <div id="qav-comment-bad-count">{isBadCount}</div>
               </div>
             </div>
           </div>
@@ -184,7 +218,7 @@ VQnAComment.propTypes = {
   isLiked: PropTypes.bool.isRequired,
   likeCount: PropTypes.number.isRequired,
   dislikeCount: PropTypes.number.isRequired,
-  triggerReload: PropTypes.func.isRequired,
+  // triggerReload: PropTypes.func.isRequired,
 };
 
 export default VQnAComment;
