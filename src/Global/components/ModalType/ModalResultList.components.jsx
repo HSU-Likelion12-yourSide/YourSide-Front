6d906097766e589ef6b20ed39710c5fe3fd37ee6
[nyun-nye] <[2001_11_7@naver.com]>
2024-11-11 18:21:56 +0900

    ✨: 내 근로지 조회 api 연동 완료

--- src/Global/components/ModalType/ModalResultList.components.jsx
+++ src/Global/components/ModalType/ModalResultList.components.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../css/ModalType/Modal.resultlist.scss';
 import useGlobalState from '../../Hooks/useGlobalState';
 import modalStateController from '../../function/modalStateController';
 import closeIcon from '../../image/modal-closeIcon.svg';
+import useFetchAPI from '../../API/Hooks/useFetchAPI';
 
 const ModalResultList = () => {
   const { isModalState, setModalState } = useGlobalState();
   const { setModalType } = useGlobalState();
+  const [isContent, setContent] = useState('');
+  const { isData, isLoading, isError, setUrl } = useFetchAPI();
+
+  // API 요청을 위한 URL 설정
+  useEffect(() => {
+    setUrl('/mypage/list/worksheet/2'); // 임시로 2로 설정
+  }, [setUrl]);
+
+  useEffect(() => {
+    if (isLoading) {
+      setContent('Loading...');
+    } else if (isError) {
+      setContent(`Error: ${isError}`);
+    } else if (isData) {
+      setContent(isData);
+    } else {
+      setContent(null);
+    }
+  }, [isLoading, isError, isData]);
 
   return (
     <div className="modal-result-list">
@@ -42,38 +62,22 @@ const ModalResultList = () => {
         </div>
       </div>
       <div id="modal-contents">
-        <div className="modal-resultbox">
-          <div className="modal-resultname">미도인 성수 근로 결과지</div>
-          <div
-            className="modal-viewdetails"
-            onKeyDown={() => {
-              console.log('test');
-            }} // 키보드 지원
-            role="button"
-            tabIndex="0"
-            onClick={() => {
-              setModalType('GetResult');
-            }}
-          >
-            자세히 보기 &gt;
-          </div>
-        </div>
-        <div className="modal-resultbox">
-          <div className="modal-resultname">미도인 성수 근로 결과지</div>
-          <div className="modal-viewdetails">자세히 보기 &gt;</div>
-        </div>
-        <div className="modal-resultbox">
-          <div className="modal-resultname">미도인 성수 근로 결과지</div>
-          <div className="modal-viewdetails">자세히 보기 &gt;</div>
-        </div>
-        <div className="modal-resultbox">
-          <div className="modal-resultname">미도인 성수 근로 결과지</div>
-          <div className="modal-viewdetails">자세히 보기 &gt;</div>
-        </div>
-        <div className="modal-resultbox">
-          <div className="modal-resultname">미도인 성수 근로 결과지</div>
-          <div className="modal-viewdetails">자세히 보기 &gt;</div>
-        </div>
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && isError && <p>Error: {isError.message}</p>}
+        {!isLoading && isData && isData.data
+          ? isData.data.slice(0, 4).map(item => (
+              <div className="modal-resultbox">
+                <div
+                  className="modal-resultname"
+                  key={item.id}
+                  id={item.worksheet_id}
+                >
+                  임시 제목
+                </div>
+                <div className="modal-viewdetails">자세히 보기 &gt;</div>
+              </div>
+            ))
+          : !isLoading && !isError && <p>No data available</p>}
       </div>
     </div>
   );
