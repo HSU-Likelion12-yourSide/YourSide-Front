40e733f9e0ac8ede2622889283437b050fbf9af1
4Bee <4bee.code@gmail.com>
2024-11-19 01:36:34 +0900

    📝: 오타 수정 및 문서화 정리

--- src/Global/API/fetch.instance.js
+++ src/Global/API/fetch.instance.js
@@ -5,10 +5,13 @@ const { REACT_APP_API_URL } = process.env;
 console.log('Environment variable:', process.env.REACT_APP_API_URL);
 
 /**
- * Axios 인스턴스를 생성하여 반환하는 함수.
  * @description
  * 이 함수는 기본 API URL을 설정하고, timeout 및 기본 header와 같은 공통 설정이 포함된 Axios instance를 생성하여 반환한다.
  * `.env` 파일에서 환경 변수 `REACT_APP_API_URL` 값을 가져와 기본 URL로 설정하며 별도로 전달된 `baseURL` 값을 사용할 수도 있다.
+ * <br>
+ * <span style="color: tomato;">
+ * 참고로 하단 property는 axios.create의 속성이므로 착오 없이 fetch.instance.js 함수와는 무관함을 인지해야 한다.
+ * </span>
  *
  * @version 1.0.0
  * @author 김동우 | 4BEE <4bee.code@gamil.com>
@@ -16,9 +19,9 @@ console.log('Environment variable:', process.env.REACT_APP_API_URL);
  * @todo Promise.all을 사용해서 직렬 Fetch를 병렬 Fetch로 변환해본다.
  *
  * @param {string} [baseURL=REACT_APP_API_URL] - API 요청의 기본 URL. 설정하지 않으면 환경 변수 `REACT_APP_API_URL`을 기본값으로 사용
- *
- * @property {number} timeout - 요청 timeout을 5000ms(5초)로 설정
- * @property {Object} headers - 기본 요청 headers를 설정, 기본적으로 `Content-Type: application/json`을 포함한다.
+ * @property {string} baseURL - (해당 속성은 axios.create의 속성입니다.) API 요청의 기본 URL
+ * @property {number} timeout - (해당 속성은 axios.create의 속성입니다.) 요청 timeout을 5000ms(5초)로 설정
+ * @property {Object} headers - (해당 속성은 axios.create의 속성입니다.) 기본 요청 headers를 설정, 기본적으로 `Content-Type: application/json`을 포함한다.
  *
  * @returns {AxiosInstance} 설정된 Axios 인스턴스를 반환한다. 이를 통해 API 요청을 손쉽게 처리할 수 있다.
  *
