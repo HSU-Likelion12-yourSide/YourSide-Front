38a121723385e096adc63dc8121ddec0b6b777b1
4Bee <4bee.code@gmail.com>
2025-02-05 23:28:22 +0900

    🔧: index.html 외 다른 파일들의 이미지 처리 확인 및 적용 단, 이미지는 모듈로 처리, 브라우저 icon은 ico 포멧으로 설정 해야한다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import "./App.css";
+import logo from "./dev/img/logo192.png";
+
 const App = () => {
   return (
-    <div>
+    <div id="title">
       <h1>Hello, Webpack with React!</h1>
+      <div>
+        <img src={logo} alt="" />
+      </div>
     </div>
   );
 };
