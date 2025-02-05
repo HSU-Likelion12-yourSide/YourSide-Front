be2198a794d27603d44befe9d4f5a5f7c3a2b225
4Bee <4bee.code@gmail.com>
2025-02-05 23:38:56 +0900

    🔧: ico 포멧 이미지 파일을 인식 할 수 있게 코드 수정

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
         type: "asset/resource",
       },
     ],
