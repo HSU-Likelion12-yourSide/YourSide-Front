f73561e1570310eef1b800dd26f119fb25594b1b
4Bee <4bee.code@gmail.com>
2025-02-06 10:53:31 +0900

    🔧: build된 결과물 위치 변경 및 copy-webpack-plugin 적용, svg 관련 최적화 미반영 따라서 svgo 플러그인 설치 필요

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
@@ -49,8 +50,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: "./index.html",
       // minify: false,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public", to: "" }],
     }),
     new CleanWebpackPlugin(),
   ],
