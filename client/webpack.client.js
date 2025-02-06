116cfd2c81b91f619165129684155a8b9b4e6a05
4Bee <4bee.code@gmail.com>
2025-02-06 16:25:28 +0900

    🔧: client로 이동된 webpack이 src를 참조할 수 있게 상대경로 설정을 절대경로로 변경, babel-loader를 대체한 ESbuild를 사용하기 위해 webpack 설정 변경 및 React 17+ 최신 문법에 맞게 bundling되게 jsx automatic 설정 추가

---
+++ client/webpack.client.js
@@ -0,0 +1,87 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { jsx } = require("react/jsx-runtime");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "bundle.js",
+    // library: "React",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    port: 3000,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        // use: {
+        //   loader: "babel-loader",
+        // },
+        use: {
+          loader: "esbuild-loader",
+          options: {
+            loader: "tsx",
+            target: "esnext",
+            jsx: "automatic",
+            // jsxFactory: "React.createElement",
+            // jsxFragment: "React.Fragment",
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        // use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svgo-loader",
+            options: {
+              plugins: [
+                {
+                  name: "removeViewBox",
+                  active: false,
+                },
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      // minify: false,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: path.resolve(__dirname, "public"), to: "" }],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
