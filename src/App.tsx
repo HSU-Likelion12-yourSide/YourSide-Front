91adc2a4231798bb94423f9ba1eb97e1180cb347
4Bee <4bee.code@gmail.com>
2025-02-06 12:01:57 +0900

    ✨: svg 적용 및 build 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import logo from "./dev/img/logo192.png";
+import check from "./dev/img/modal-check.svg";
 
 const App = () => {
   return (
     <div id="title">
       <h1>Hello, Webpack with React!</h1>
       <div>
+        <img src={check} alt="" width="100px" height="auto" />
         <img src={logo} alt="" />
       </div>
     </div>
